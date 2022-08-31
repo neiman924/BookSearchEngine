b8bed1888e73855444a97c12cca0bdd1b7c50d58
Hossein Neiman <neiman924@gmail.com>
2022-08-30 20:55:26 -0700

    ready to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-mongoose.connect("mongodb+srv://neiman924:a2z7GfKBcb77!GH@cluster0.lejbeog.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
 module.exports = mongoose.connection;
